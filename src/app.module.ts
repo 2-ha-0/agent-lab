8c1b974c74ca66eafc7fff53ad4cf6d053658eb3
2-h-0 <alohaquarium@gmail.com>
2026-07-21 18:21:57 +0900

    chore: ollama module, service 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { EmbeddingService } from './embedding/embedding.service';
 import { EmbeddingController } from './embedding/embedding.controller';
+import { OllamaModule } from './ollama/ollama.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    OllamaModule,
   ],
   controllers: [AppController, EmbeddingController],
   providers: [AppService, EmbeddingService],
