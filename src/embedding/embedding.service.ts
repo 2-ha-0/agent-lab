470617738c4b1e2c614345a0d612621768ad202e
2-h-0 <alohaquarium@gmail.com>
2026-07-21 17:37:00 +0900

    feat: embedding test service 작성

--- src/embedding/embedding.service.ts
+++ src/embedding/embedding.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { OpenAI } from 'openai';
 
 @Injectable()
-export class EmbeddingService {}
+export class EmbeddingService {
+  private readonly openai: OpenAI;
+
+  constructor(private configService: ConfigService) {
+    this.openai = new OpenAI({
+      apiKey: this.configService.get<string>('OPENAI_API_KEY'),
+    });
+  }
+
+  async createEmbedding(text: string) {
+    const response = await this.openai.embeddings.create({
+      model: 'text-embedding-3-small',
+      input: text,
+    });
+
+    return response.data[0].embedding;
+  }
+}
